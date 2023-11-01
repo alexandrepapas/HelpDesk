024a3cd3d1e6156c0c9fe26b74501baf4fd14fd5
alexandrepapas <alehyxd@gmail.com>
2023-11-01 16:44:28 -0300

    criado enuns

---
+++ src/main/java/com/alexandreHelpDesk/domains/enuns/Perfil.java
@@ -0,0 +1,41 @@
+package com.alexandreHelpDesk.domains.enuns;
+
+public enum Perfil {
+
+    ADMIN (0,"ROLE_ADMIN"),
+    CLIENTE (1,"ROLE_CLIENT"),
+    TECNICO(2,"ROLE_TECNICO");
+
+
+    private Integer codigo;
+    private String descricao;
+
+    Perfil(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Perfil toEnum(Integer cod){
+        if(cod ==null){
+            return  null;
+        }
+
+        for(Perfil x: Perfil.values()){
+            if(cod.equals(x.getCodigo())){
+                return  x;
+            }
+        }
+        throw  new IllegalArgumentException("Perfil Inválido");
+    }
+
+}
