8b7a7c14f2b01247909a90776617954fabb5ea79
alexandrepapas <alehyxd@gmail.com>
2023-11-09 11:50:10 -0300

    endpoint findyAll Tecnicos

--- src/main/java/com/alexandreHelpDesk/resources/TecnicoResource.java
+++ src/main/java/com/alexandreHelpDesk/resources/TecnicoResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value="/tecnicos")
 public class TecnicoResource {
@@ -21,6 +24,13 @@ public class TecnicoResource {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
         Tecnico obj= tecnicoService.findById(id);
         return  ResponseEntity.ok().body(new TecnicoDTO(obj));
+    }
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findall(){
+        List<Tecnico> list= tecnicoService.findAll();
+        List<TecnicoDTO> listDto= list.stream().map(obj-> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+
     }
 
 }
--- src/main/java/com/alexandreHelpDesk/services/TecnicoService.java
+++ src/main/java/com/alexandreHelpDesk/services/TecnicoService.java
@@ -6,6 +6,7 @@ import com.alexandreHelpDesk.repositories.TecnicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,7 @@ public class TecnicoService {
         return obj.orElseThrow(()-> new ObjectNotFoundExecpetion("Objeto não encontrado id: "+id));
     }
 
-
-
+    public List<Tecnico> findAll() {
+        return  tecnicoRepository.findAll();
+    }
 }
